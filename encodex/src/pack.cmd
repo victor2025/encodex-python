10e0dd18672f81c4e6e8da72c27ff4c2ad60d05e
VICTOR2022_KB502 <lihengwei111@outlook.com>
2022-06-21 18:38:38 +0800

    添加了cmd打包脚本，添加了win环境的release版本

---
+++ encodex/src/pack.cmd
@@ -0,0 +1,20 @@
+::编译打包
+pyinstaller -F encodex.py
+rm -r ./build encodex.spec
+:: 移动文件
+set dirname=encodex-win-amd64-%1%
+mkdir %dirname%
+mv ./dist/encodex %dirname%/encodex
+rm -r dist
+copy config.ini %dirname%
+:: 压缩
+tar -cf %dirname%.tar.gz %dirname%
+:: 移动压缩包
+mv %dirname%.tar.gz %dirname%/%dirname%.tar.gz
+:: # 移动文件夹
+set para=%1%
+set home=%cd%
+cd ../release
+mkdir %para%
+cd %home%
+mv %dirname% ../release/%para%/%dirname%
--- encodex/src/pack.sh
+++ encodex/src/pack.sh
@@ -2,7 +2,7 @@
 pyinstaller -F encodex.py
 rm -r ./build encodex.spec
 # 移动文件
-dirname="encodex-linux-amd64-$1"
+dirname=encodex-linux-amd64-$1
 mkdir $dirname
 mv ./dist/encodex $dirname/encodex
 rm -r dist
@@ -12,4 +12,5 @@ tar -czvf $dirname.tar.gz $dirname
 # 移动压缩包
 mv $dirname.tar.gz ./$dirname/$dirname.tar.gz
 # 移动文件夹
-mv $dirname ../release/$dirname
+mkdir ../release/$1
+mv $dirname ../release/$1/$dirname
